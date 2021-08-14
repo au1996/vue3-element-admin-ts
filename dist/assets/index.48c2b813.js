9a970764d4a71770f2f0ab33edf73bbe44675ddc
au1996 <15119376155@163.com>
2021-08-14 16:30:36 +0800

    feat: :zap: update vue version

--- dist/assets/index.48c2b813.js
+++ dist/assets/index.48c2b813.js
@@ -1,4 +1,4 @@
-import{d as t,b as e,o as n,W as i,c as r}from"./vue.23ad2e95.js";
+import{d as t,b as e,o as n,W as i,c as r}from"./vue.52705917.js";
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
---
+++ dist/assets/menu11.4ccee326.js
@@ -0,0 +1,1 @@
+import{W as r,c as e}from"./vue.52705917.js";const n={};n.render=function(n,t){return r(),e("h1",null,"菜单1-1")};export default n;
