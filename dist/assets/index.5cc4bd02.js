a36b322c139c3a1647d8c65ef37445f8ee35177d
gujinzhu <15119376155@163.com>
2021-11-29 15:26:54 +0800

    feat: :zap: update stylelintrc

--- dist/assets/index.5cc4bd02.js
+++
@@ -1,1 +0,0 @@
-import{d as a,o as s,c as e,v as t,a6 as o,a7 as c}from"./vue.7ec5d134.js";o("data-v-44be0af4");const d={class:"title"};c();var p=a({expose:[],setup:a=>(a,o)=>(s(),e("h1",d,t("雪月测试 scss 全局变量")))});p.__scopeId="data-v-44be0af4";export{p as default};
