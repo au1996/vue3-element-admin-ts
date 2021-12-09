37222aad30b68b614e86365d21858ed945624a77
gujinzhu <15119376155@163.com>
2021-12-09 20:42:02 +0800

    feat: :bookmark: init webpack version

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,19 @@ import './permission'
 // utils
 import { DateFormat } from './utils/util'
 
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
+}
+
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(Directives)
