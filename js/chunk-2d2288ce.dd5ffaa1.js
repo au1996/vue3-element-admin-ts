346f3c166761c2fc8edc0426b5d7f3d4a199a442
gujinzhu <15119376155@163.com>
2022-04-15 11:49:39 +0800

    feat: :arrow_up: upgraded vue version

--- js/chunk-2d2288ce.dd5ffaa1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d2288ce"],{da50:function(n,c,e){"use strict";e.r(c);var o=e("5c40"),t=e("6b0d");const r={},u=e.n(t)()(r,[["render",function(n,c){return Object(o.openBlock)(),Object(o.createBlock)("h1",null,"菜单2")}]]);c.default=u}}]);
