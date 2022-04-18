6332339a795809904df93610fc58afe614549ba6
au1996 <15119376155@163.com>
2022-04-18 19:50:58 +0800

    feat: build

---
+++ dist/js/chunk-2d2288ce.d36f01ef.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d2288ce"],{da50:function(n,e,c){"use strict";c.r(e);var t=c("5c40"),o=c("6b0d");const r={},u=c.n(o)()(r,[["render",function(n,e){return Object(t.openBlock)(),Object(t.createElementBlock)("h1",null,"菜单2")}]]);e.default=u}}]);
