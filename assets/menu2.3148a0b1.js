5c5cea575e5e4aa6f112e478561aa0e7105e132f
au1996 <au1996@users.noreply.github.com>
2022-11-12 08:53:13 +0000

    deploy e15757c6920ea8117a80a22427fd8a380d72eeb4

---
+++ assets/menu2.3148a0b1.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.d47ca7f1.js";import{f as r,h as s}from"./vue.ba1d3062.js";import"./element-plus.ccb277d2.js";const t=e({},[["render",function(e,t){return r(),s("h1",null,"菜单2")}]]);export{t as default};
